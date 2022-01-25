3bd841156b55482b0a309d463aec92bdd5125bad
Peter Shen <xianpeng.shen@gmail.com>
2022-01-25 12:38:53 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include "foo.h"
- 
+
+#ifdef X
+#define Y "X DEFINED"
+#else
+#define Y "X NOT DEFINED"
+#endif
+
 int main(void)
 {
     printf("Start calling foo() ...\n");
     foo(1);
     foo(2);
+    if (0)
+    {
+        printf("skip this line.");
+    }
     return 0;
 }
