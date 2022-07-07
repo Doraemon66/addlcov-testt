71465afd40b6fea7d55a6b9cb34a0a808e83eb86
Doraemon66 <39914789+Doraemon66@users.noreply.github.com>
2022-07-07 16:33:17 +0800

    Update foo.c

--- foo.c
+++ foo.c
@@ -13,3 +13,14 @@ void foo(int num)
 	    printf("ok\n");
     }
 }
+
+void foo2(int num)
+{
+    if (num == 3) {
+        printf("when num is equal to 3...\n");
+    } else if (num == 4){
+        printf("when num is equal to 4...\n");
+    } else {
+        printf("when num is equal to %d...\n", num);
+    }
+}
