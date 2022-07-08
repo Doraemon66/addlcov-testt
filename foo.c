dbd3a46bec3646677288060f70812d35679b80f8
Doraemon66 <935616614@qq.com>
2022-07-08 11:03:03 +0800

    origin

--- foo.c
+++ foo.c
@@ -14,17 +14,6 @@ void foo(int num)
     }
 }
 
-void foo3(int num)
-{
-    if (num == 3) {
-        printf("when num is equal to 5...\n");
-    } else if (num == 4){
-        printf("when num is equal to 6...\n");
-    } else {
-        printf("when num is equal to %d...\n", num);
-    }
-}
-
 void foo2(int num)
 {
     if (num == 3) {
@@ -36,4 +25,13 @@ void foo2(int num)
     }
 }
 
-
+void foo3(int num)
+{
+    if (num == 3) {
+        printf("when num is equal to 3...\n");
+    } else if (num == 4){
+        printf("when num is equal to 4...\n");
+    } else {
+        printf("when num is equal to %d...\n", num);
+    }
+}
